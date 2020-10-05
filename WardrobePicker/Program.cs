d18b6ba45d818f2d130f6a63a6af1c33809d258a
Opilane TTHK <opilane@tthk.ee>
2020-10-05 14:50:40 +0300

    Week6Array

---
+++ WardrobePicker/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace WardrobePicker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] wardrobe = {"hat", "shirt", "jeans", "hoodie", "pants"};
+            string[] colors = {"green", "pink", "red", "blue", "yellow"};
+            string[] patterns = { "flowery", "striped", "checked", "polka dot", "cirkles" };
+
+        }
+    }
+}
---
+++ Week6Arrays/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Week6Arrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] predictions = { "win a million", "fall in love", "buy youtube premium ", "join the dark side" };
+            //Today you will...
+            Random rnd = new Random();
+            int randomIndex = rnd.Next(1, predictions.Lenght);
+            Console.WriteLine($"Today you will {predictions[randomIndex]}");
+
+        }
+    }
+}
