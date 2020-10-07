5e78f326bcdef492cbc5835809b889a3aa9be9a6
Opilane TTHK <opilane@tthk.ee>
2020-10-07 16:35:05 +0300

    Week6Arrays

--- UserAgainstZombies/Program.cs
+++ UserAgainstZombies/Program.cs
@@ -6,7 +6,18 @@ namespace UserAgainstZombies
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string[] userBag = new string[5];
+            int i = 0;
+            while(i < userBag.Length)
+            {
+                Console.WriteLine("What's in your bag?");
+                userBag[i] = Console.ReadLine();
+                i++;
+            }
+            Random rnd = new Random();
+            Console.WriteLine($"In case of zombie appocalypse you will have to" +
+                $"fight zombies off with a {userBag[rnd.Next(0,userBag.Length)]}");
+
         }
     }
 }
--- WardrobePicker/Program.cs
+++ WardrobePicker/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Resources;
 
 namespace WardrobePicker
 {
@@ -9,7 +10,26 @@ namespace WardrobePicker
             string[] wardrobe = {"hat", "shirt", "jeans", "hoodie", "pants"};
             string[] colors = {"green", "pink", "red", "blue", "yellow"};
             string[] patterns = { "flowery", "striped", "checked", "polka dot", "cirkles" };
+            string randomCothes
+                string randomWardrope
+                string randomPatterns
 
+                Random rnd = new Random();
+
+            Console.WriteLine("would you like some fashion advice?");
+            string userInput = Console.ReadLine();
+
+            if(userInput == "no")
+            {
+                Console.WriteLine("Farewell");
+            }
+            else
+            {
+                Console.WriteLine($"Today, you should wear" + 
+                    $"{colors[rnd.Next(0,colors.Length]}" + 
+                    $"{patterns[rnd.Next(0, patterns.Length]}" + 
+                    $"{wardrobe[rnd.Next(0, wardrobe.Length]}");
+            }
         }
     }
 }
